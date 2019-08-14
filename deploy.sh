22792497761d018cfef753bcf79740ca5a5f1cd5
Omar MEBARKI <omar.mebarki@gmail.com>
2019-08-14 22:21:55 +0200

    Init Project

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,43 @@
 typeset SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 function get_resource(){
     typeset rtype=$1
-    typseset rname=$2
+    typeset rname=$2
     oc get ${rtype} ${rname} -o name --ignore-not-found=true
 }
 
-if [[ -z "$(get_resource cm multicontainer-server-config)" ]];
-then
-    oc create -f ${SCRIPT_DIR}/multicontainer-server/target/openshift/configMap.yml
-fi
+#server
+oc import-image multicontainer-server:0.0.1-SNAPSHOT --from=docker.io/tadartiw/multicontainer-server:0.0.1-SNAPSHOT --confirm --insecure=true
 
-if [[ -z "$(get_resource cm multicontainer-client-config)" ]];
+if [[ -n "$(get_resource cm multicontainer-server-config)" ]];
 then
-    oc create -f ${SCRIPT_DIR}/multicontainer-client/target/openshift/configMap.yml
+   oc delete cm  multicontainer-server-config
 fi
+oc create -f ${SCRIPT_DIR}/multicontainer-server/target/openshift/configMap.yml
+
+
+#client
+oc import-image multicontainer-client:0.0.1-SNAPSHOT --from=docker.io/tadartiw/multicontainer-client:0.0.1-SNAPSHOT --confirm --insecure=true
+if [[ -n "$(get_resource cm multicontainer-client-config)" ]];
+then
+    oc delete cm  multicontainer-client-config
+fi
+oc create -f ${SCRIPT_DIR}/multicontainer-client/target/openshift/configMap.yml
+
+if [[ -n "$(get_resource svc multicontainer-client)" ]];
+then
+    oc delete svc  multicontainer-client
+fi
+oc create -f ${SCRIPT_DIR}/multicontainer-client/target/openshift/service.yml
+
+if [[ -n "$(get_resource route multicontainer-client-route)" ]];
+then
+    oc delete route  multicontainer-client-route
+fi
+oc create -f ${SCRIPT_DIR}/multicontainer-client/target/openshift/route.yml
+
+#DC
+if [[ -n "$(get_resource dc multicontainer-pod)" ]];
+then
+    oc delete dc  multicontainer-pod
+fi
+oc create -f ${SCRIPT_DIR}/target/openshift/deploymentConfig.yml
--- multicontainer-client/src/main/java/omar/mebarki/multicontainerpod/ClientController.java
+++ multicontainer-client/src/main/java/omar/mebarki/multicontainerpod/ClientController.java
@@ -1,5 +1,6 @@
 package omar.mebarki.multicontainerpod;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,11 @@ import org.springframework.web.client.RestTemplate;
 public class ClientController {
     private RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${server.hello.url}")
+    private String serverURL;
+
     @RequestMapping("/client")
     public String client() {
-        return restTemplate.getForEntity("http://127.0.0.1:8081/hello", String.class).getBody();
+        return restTemplate.getForEntity(serverURL, String.class).getBody();
     }
 }
